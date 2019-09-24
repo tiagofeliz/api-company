11963c4ca0cb15add1c4bfd64bc317c08320b419
tiago.oliveira <tiago.oliveira@hotmart.com>
2019-09-24 10:43:50 -0300

    address endpoints

--- src/main/java/br/com/hotmart/company/service/AddressServiceImpl.java
+++ src/main/java/br/com/hotmart/company/service/AddressServiceImpl.java
@@ -6,6 +6,10 @@ import br.com.hotmart.company.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Component
 public class AddressServiceImpl implements AddressService {
 
@@ -13,7 +17,40 @@ public class AddressServiceImpl implements AddressService {
     private AddressRepository addressRepository;
 
     @Override
+    public List<AddressDto> findAll() {
+        List<Address> addresses = addressRepository.findAll();
+        return addresses.stream().map(AddressDto::new).collect(Collectors.toList());
+    }
+
+    @Override
     public AddressDto create(Address address) {
         return new AddressDto(addressRepository.save(address));
+    }
+
+    @Override
+    public AddressDto update(Address address, Long id) {
+        Optional<Address> addressOptional = addressRepository.findById(id);
+        if(addressOptional.isPresent()) {
+            save(addressOptional.get(), address);
+            return new AddressDto(addressOptional.get());
+        }else{
+            throw new RuntimeException("Address not found");
+        }
+    }
+
+    public void save(Address currentAddress, Address toSaveAddress) {
+        currentAddress.setCountry(toSaveAddress.getCountry());
+        currentAddress.setCity(toSaveAddress.getCity());
+        currentAddress.setStreet(toSaveAddress.getStreet());
+        currentAddress.setUf(toSaveAddress.getUf());
+        currentAddress.setZipCode(toSaveAddress.getZipCode());
+    }
+
+    public Optional<AddressDto> findById(Long id) {
+        Optional<Address> addressOptional = addressRepository.findById(id);
+        if(addressOptional.isPresent()){
+            return Optional.of(new AddressDto(addressOptional.get()));
+        }
+        return Optional.empty();
     }
 }
--- src/test/java/br/com/hotmart/company/service/AddressServiceImplTest.java
+++ src/test/java/br/com/hotmart/company/service/AddressServiceImplTest.java
@@ -1,6 +1,7 @@
 package br.com.hotmart.company.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import br.com.hotmart.company.model.dto.AddressDto;
 import br.com.hotmart.company.model.entity.Address;
@@ -9,7 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AddressServiceImplTest {
@@ -21,12 +27,78 @@ public class AddressServiceImplTest {
     private AddressRepository addressRepository;
 
     @Test
+    public void shouldReturnAEmptyListWhenThereNoAreRecords(){
+        List<Address> addressList = new ArrayList<>();
+        Mockito.when(addressRepository.findAll()).thenReturn(addressList);
+        List<AddressDto> addresses = addressService.findAll();
+
+        assertTrue(addresses.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnAListOfAddressesWhenThereAreRecords(){
+        Address address = new Address(
+                "Brasil",
+                "MG",
+                "BH",
+                "Entre Rios",
+                "30710-080");
+
+        List<Address> addressList = new ArrayList<>();
+        addressList.add(address);
+
+        Mockito.when(addressRepository.findAll()).thenReturn(addressList);
+        List<AddressDto> addresses = addressService.findAll();
+
+        assertEquals(1, addresses.size());
+        assertEquals(address.getStreet(), addresses.get(0).getStreet());
+    }
+
+    @Test
     public void shouldSaveAddress(){
         Address address = new Address("Brasil", "MG", "BH", "Entre Rios", "30710-080");
+
+        Mockito.when(addressRepository.save(address)).thenReturn(address);
 
         AddressDto savedAddress = addressService.create(address);
 
         assertEquals(address.getStreet(), savedAddress.getStreet());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowAExceptionWhenAddressIdIsNotValid(){
+        Address address = new Address("Brasil", "MG", "BH", "Entre Rios", "30710-080");
+        Mockito.when(addressRepository.findById(1L)).thenReturn(Optional.empty());
+
+        addressService.update(address, 1L);
+    }
+
+    @Test
+    public void shouldUpdateAAddress(){
+        Address currentAddress = new Address("Brasil", "MG", "BH", "Entre Rios", "30710-080");
+        Address updateTo = new Address("Japao", "MG", "BH", "Entre Rios", "30710-080");
+        Mockito.when(addressRepository.findById(1L)).thenReturn(Optional.of(currentAddress));
+
+        AddressDto updatedAddress = addressService.update(updateTo, 1L);
+
+        assertEquals(updateTo.getStreet(), updatedAddress.getStreet());
+    }
+
+    @Test
+    public void shouldReturnAAddress(){
+        Address address = new Address(
+                "Brasil",
+                "MG",
+                "BH",
+                "Entre Rios",
+                "30710-080");
+        address.setId(1L);
+
+        Mockito.when(addressRepository.findById(1L)).thenReturn(Optional.of(address));
+
+        Optional<AddressDto> newEmployee = addressService.findById(1L);
+
+        assertEquals(address.getStreet(), newEmployee.get().getStreet());
     }
 
 }
